8455bd22fac01c4ae0238232a162c7e7ca80a0bf
scy6500 <scy6500@kookmin.ac.kr>
2021-08-29 23:02:24 +0900

    feat: connect with server

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import requests
+import base64
+from PIL import Image
+from io import BytesIO
+import json
+
+
+def send_request(text, num_images):
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    data = {"text": text, "num_images": num_images}
+
+    response = requests.post('https://main-dalle-server-scy6500.endpoint.ainize.ai/generate', headers=headers,
+                             data=json.dumps(data))
+    status_code = response.status_code
+
+    return status_code, response
+
+st.title("DALL-E Pytorch Demo")
+st.header("Creating Images from Text")
+
+num_images = st.sidebar.slider("Number of images you want", 1, 4)
+
+text = st.text_input("Text for the image you want to create", "an armchair in the shape of an avocado")
+if st.button("Generate"):
+    t = st.empty()
+    result = []
+    t.markdown("Generating...")
+    status_code, response = send_request(text, num_images)
+    if status_code == 200:
+        prediction = response.json()
+        for base64_image in prediction:
+            result.append(Image.open(BytesIO(base64.b64decode(base64_image))))
+        st.image(result, width=300)
+    else:
+        st.error(str(status_code) + " Error")
+    t.markdown("")
